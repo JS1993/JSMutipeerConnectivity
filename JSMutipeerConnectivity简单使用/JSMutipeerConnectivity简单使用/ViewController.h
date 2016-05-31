e3985f17954dcdd98397bfb04ed74c6d74b5aca9
susu <28025920@qq.com>
2016-05-31 14:15:41 +0800

    完成界面布局

---
+++ JSMutipeerConnectivity简单使用/JSMutipeerConnectivity简单使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSMutipeerConnectivity简单使用
+//
+//  Created by  江苏 on 16/5/31.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSMutipeerConnectivity简单使用/JSMutipeerConnectivity简单使用/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  JSMutipeerConnectivity简单使用
+//
+//  Created by  江苏 on 16/5/31.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *showImageV;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+- (IBAction)connectDevice:(id)sender {
+}
+- (IBAction)chooseImage:(id)sender {
+}
+- (IBAction)sendImage:(id)sender {
+}
+- (IBAction)isCanBeFound:(id)sender {
+}
+
+
+
+@end
